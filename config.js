1af99eac298d12976a8c5b54bafeb8da0caccc93
kev127 <kelvinkeya125@gmail.com>
2022-12-05 17:44:16 +0300

    add config file

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://kev125:Rf7ebDwjS8gRHatM@yolo.1kvi6ui.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://kev125:Rf7ebDwjS8gRHatM@yolo.1kvi6ui.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://kev125:Rf7ebDwjS8gRHatM@yolo.1kvi6ui.mongodb.net/?retryWrites=true&w=majority',
+}
+module.exports = config;
+ 
